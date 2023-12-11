41f2324b7c48b0282833f34240bc79ec41197ede
AnderOlivos01 <andersonolivos01@iesamachado.org>
2023-12-11 19:50:54 +0100

    modeloExamen ejercicio 2 hecho

---
+++ modeloExamen/ejercicio1/ejercicio1.js
@@ -0,0 +1,6 @@
+validar();
+
+
+function validar(){
+
+}
---
+++ modeloExamen/ejercicio3/ejercicio3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3</title>
+</head>
+<body>
+    <h1>Email PHP</h1>
+    <form action="" method="get">
+        <label for="">Asunto</label>
+        <textarea name="asunto" id="asunto" cols="30" rows="10"></textarea>
+        <label for="">Destinatario</label>
+        <input type="text">
+    </form>
+</body>
+</html>
