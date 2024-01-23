546efefcbea51d6dd9600ba80d8ce8b320a3f0af
Anderson Olivos <andersonolivos01@iesamachado.org>
2024-01-23 14:28:13 +0100

    repaso PHP

---
+++ 2Trimestre/repasoPHP/conexion.php
@@ -0,0 +1,7 @@
+<?php
+$hostname="sql213.thsite.top";
+$username="thsi_35529605";
+$password="";
+$database="thsi_35529605_repasoPHP";
+$mysqli = new mysqli($hostname,$username,$password,$database);
+?>
