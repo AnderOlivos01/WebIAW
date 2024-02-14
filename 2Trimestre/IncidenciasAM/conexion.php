fc9808eae8a05ba58bf1ebc9e095b826b34a287e
AnderOlivos01 <andersonolivos01@iesamachado.org>
2024-02-14 13:52:57 +0100

    actualizado

--- 2Trimestre/IncidenciasAM/conexion.php
+++ 2Trimestre/IncidenciasAM/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $hostname="sql213.thsite.top";
 $username="thsi_35529605";
-$password="4i!HfMrP";
+$password="4i!";
 $database="thsi_35529605_proyecto1";
 $mysqli = new mysqli($hostname,$username,$password,$database);
 ?>
